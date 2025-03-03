119a5f62d93c6b9ed4b9b91426e37bc7d75524b7
Alex Scarborough <alex@teak.io>
2025-03-03 15:59:51 -0800

    Initial Commit

---
+++ TeakSwiftCleanroomPods/TeakSwiftCleanroomPodsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TeakSwiftCleanroomPodsApp.swift
+//  TeakSwiftCleanroomPods
+//
+//  Created by Alexander Scarborough on 3/3/25.
+//
+
+import SwiftUI
+
+@main
+struct TeakSwiftCleanroomPodsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
